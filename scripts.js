ed37bff5cf47142cdcfe8a0439c9b35c1e39c49b
prasetion <prasetion@gmail.com>
2024-06-18 20:01:42 +0700

    orbit control by native js

---
+++ scripts.js
@@ -0,0 +1,82 @@
+import * as THREE from "three";
+
+// canvas
+const canvas = document.querySelector("canvas.webgl");
+
+// cursor
+const cursor = {
+  x: 0,
+  y: 0,
+};
+
+window.addEventListener("mousemove", (event) => {
+  cursor.x = event.clientX / sizes.width - 0.5;
+  cursor.y = -(event.clientY / sizes.height - 0.5);
+  console.log(cursor.x, cursor.y);
+});
+
+// scene
+const scene = new THREE.Scene();
+
+// object
+const geometry = new THREE.BoxGeometry(1, 1, 1);
+const material = new THREE.MeshBasicMaterial({ color: 0xff0000 });
+const mesh = new THREE.Mesh(geometry, material);
+scene.add(mesh);
+
+// set position
+/**
+ * Sizes
+ */
+const sizes = {
+  width: 800,
+  height: 600,
+};
+
+/**
+ * Camera
+ */
+const camera = new THREE.PerspectiveCamera(
+  75,
+  sizes.width / sizes.height,
+  1,
+  1000
+);
+// const aspectRatio = sizes.width / sizes.height;
+// const camera = new THREE.OrthographicCamera(
+//   -1 * aspectRatio,
+//   1 * aspectRatio,
+//   1,
+//   -1,
+//   0.1,
+//   100
+// );
+camera.position.z = 3;
+
+// lookat
+// camera.lookAt(new THREE.Vector3(0, -1, 0));
+// camera.lookAt(mesh.position);
+scene.add(camera);
+
+/**
+ * Renderer
+ */
+const renderer = new THREE.WebGLRenderer({
+  canvas: canvas,
+});
+
+renderer.setSize(sizes.width, sizes.height);
+
+const tick = () => {
+  // update camera
+  camera.position.x = Math.sin(cursor.x * Math.PI * 2) * 2;
+  camera.position.z = Math.cos(cursor.x * Math.PI * 2) * 2;
+  camera.position.y = cursor.y * 3;
+  camera.lookAt(mesh.position);
+
+  // render per frame
+  renderer.render(scene, camera);
+  window.requestAnimationFrame(tick);
+};
+
+tick();
